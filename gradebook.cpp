a8ccd73ad9eb6aae49c6b039892186126c8d211b
Matthew <69696416+mattcordeiro@users.noreply.github.com>
2023-02-08 14:11:26 -0500

    Add files via upload

--- gradebook.cpp
+++ gradebook.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "gradebook.h"
+void examCalculation(std::vector<int> exam, int finalExamScore){
+    
+
+}
+
+
+void ReadFile(std::string file_name, std::vector<std::vector<double>> * image_data){
+
+    // Opens the file for reading
+    std::ifstream file(file_name);
+    
+    // Creates a string to hold each line in temporarily
+    std::string str;
+    
+    // Iterates over the file, storing one line at a time into `str`
+    while (std::getline(file, str)) {
+        
+        // Create a temporary 1D Vector of doubles
+        std::vector<double> new_row;
+    
+        // Create a stringstream object with our line of integers from the file
+        std::istringstream ss(str);
+    
+        // Create a double that will hold our extracted value from the string
+        double token;
+    
+        // While there are still numbers in this string, extract them as doubles
+        while(ss >> token){
+            // Push these doubles into our temp vector
+            new_row.push_back(token);
+        }
+    
+        // The line is empty, push our completed row into our 2D vector
+        image_data->push_back(new_row);
+    }
+}
+void WriteFile(std::string file_name, std::vector<std::vector<double>> * const image_data){
+    
+    // Open our file with write permissions
+    std::ofstream output_file(file_name);
+    
+    // Use an iterator to traverse the outer dimension
+    for(std::vector<std::vector<double>>::const_iterator i = (*image_data).begin(); i != (*image_data).end(); ++i){
+        
+        // Use an interator to traverse the inner dimention
+        for(std::vector<double>::const_iterator j = i->begin(); j != i->end(); ++j){
+            
+            // Output each element to the file with a space at the end
+            output_file << *j << " ";
+       
+         }
+       
+         // Output a newline symbol to the file
+        output_file << "\n";
+    }
+}
--- gradebook.h
+++ gradebook.h
@@ -4,14 +4,50 @@
 
 #ifndef REVIEW_PROJECT_GRADEBOOK_H
 #define REVIEW_PROJECT_GRADEBOOK_H
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 
 class gradebook {
     //things that will not be accessed explicitly
 private:
+    std::vector<int> labs;
+    std::vector<int> assignments;
+    std::vector<int> projects;
+    std::vector<int> exam;
     //things that will be accessed explicitly
 public:
+    //constructor holder
+    gradebook::gradebook(); 
+
+    //void calculate labs
+    void labCalculation(std::vector<int> labs, int finalLabScore);
+    
+    //void calculate assignments
+    void assignmentCalculation(std::vector<int> assignments, int finalAssignmentScore);
+
+    //void calculate projects
+    void projectCalculation(std::vector<int> project, int finalProjectScore);
+
+    //void calculate exam
+    void examCalculation(std::vector<int> exam, int finalExamScore);
+
+    //read file
+    void ReadFile(std::string file_name, std::vector<std::vector<double>> * image_data);
+    //write file
+    void WriteFile(std::string file_name, std::vector<std::vector<double>> * const image_data);
 };
 
 
 #endif //REVIEW_PROJECT_GRADEBOOK_H
+
+//Notes from meeting
+/*
+Dan - data intake or format
+
+Built for CSC 212;
+
+Start off with initial file; add to a list of file references
+new, load, .txt; file CLA; 
+*/
