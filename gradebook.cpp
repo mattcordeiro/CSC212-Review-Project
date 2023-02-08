ea6f0c32cccc8c4a43313d8bac0564231ddcd7d1
Matthew <69696416+mattcordeiro@users.noreply.github.com>
2023-02-08 13:59:27 -0500

    Add files via upload

---
+++ gradebook.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matt cordeiro on 2/8/23.
+//
+
+#include "gradebook.h"
---
+++ gradebook.h
@@ -0,0 +1,17 @@
+//
+// Created by matt cordeiro on 2/8/23.
+//
+
+#ifndef REVIEW_PROJECT_GRADEBOOK_H
+#define REVIEW_PROJECT_GRADEBOOK_H
+
+
+class gradebook {
+    //things that will not be accessed explicitly
+private:
+    //things that will be accessed explicitly
+public:
+};
+
+
+#endif //REVIEW_PROJECT_GRADEBOOK_H
