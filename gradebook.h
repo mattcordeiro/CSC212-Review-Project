1b9e7072bab377dcb0e66749fe834be53e730b5c
DRRoche <123427914+DRRoche@users.noreply.github.com>
2023-02-08 14:32:34 -0500

    added vector for categories
    Push is intended to familiarize with github.

--- gradebook.h
+++ gradebook.h
@@ -4,10 +4,13 @@
 
 #ifndef REVIEW_PROJECT_GRADEBOOK_H
 #define REVIEW_PROJECT_GRADEBOOK_H
-
+#include <vector>
 
 class gradebook {
     //things that will not be accessed explicitly
+    std::vector<int> labs;
+    std::vector<int> assignments;
+    std::vector<int> projects;
 private:
     //things that will be accessed explicitly
 public:
