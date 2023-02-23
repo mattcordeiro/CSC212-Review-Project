076c37cbc6f2770f8361435a6520a6f057ff9d4a
Matthew <69696416+mattcordeiro@users.noreply.github.com>
2023-02-23 10:04:48 -0500

    commented in files completed, will merge with new main adn then copy over to my branch

--- Review-Project/Review-Project/Gradebook.h
+++ Review-Project/Review-Project/Gradebook.h
@@ -3,8 +3,11 @@
 class Gradebook
 {
 private:
+    //gradebook has a private name and a vector of categories that can include exams, assignments, labs and any other grading criteria.
 	std::string name;
 	std::vector<Catagory> catagories;
+
+    //Different options for our gradebook such as Getting the name of a gradebook, creating a category and finding its location, in addition to adding and deleting a category
 public:
 	Gradebook(std::string name);
 	std::string GetName();
